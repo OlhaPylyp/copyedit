0d270ef3b5c49c66760c9f8aaf5ec625df55cbd7
OlhaPylyp <olhasvet@gmail.com>
2021-10-18 20:23:08 +0300

    Updates

---
+++ precache-manifest.f72974dd74b77cf767cf141e3e86be2c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7736764e19498a32fffc8983b609608",
+    "url": "/copyedit/index.html"
+  },
+  {
+    "revision": "8499334449408a2bc0aa",
+    "url": "/copyedit/static/css/main.341da6e2.chunk.css"
+  },
+  {
+    "revision": "f41d8765f3c4b65e9028",
+    "url": "/copyedit/static/js/2.ee453413.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/copyedit/static/js/2.ee453413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8499334449408a2bc0aa",
+    "url": "/copyedit/static/js/main.46a8079a.chunk.js"
+  },
+  {
+    "revision": "336ff3dab55e8dbb15f9",
+    "url": "/copyedit/static/js/runtime-main.ae7a9173.js"
+  },
+  {
+    "revision": "1a7e0a8165eb339f840ae337801e6307",
+    "url": "/copyedit/static/media/GothamPro-Bold.1a7e0a81.woff2"
+  },
+  {
+    "revision": "45aa54635ee87503742d3b4af08a808f",
+    "url": "/copyedit/static/media/GothamPro-Bold.45aa5463.ttf"
+  },
+  {
+    "revision": "837d943f9b8073ebf5644fe77eec1374",
+    "url": "/copyedit/static/media/GothamPro-Bold.837d943f.woff"
+  },
+  {
+    "revision": "9463f6b50bdf54c2432537a1883349c4",
+    "url": "/copyedit/static/media/GothamPro-Bold.9463f6b5.eot"
+  },
+  {
+    "revision": "974b1ea716f8b1f510481ac79d1561d8",
+    "url": "/copyedit/static/media/GothamPro-Bold.974b1ea7.svg"
+  },
+  {
+    "revision": "0258eec63236ae7fe2de0565d87f21e9",
+    "url": "/copyedit/static/media/Verdana-Bold.0258eec6.svg"
+  },
+  {
+    "revision": "215accaad3dd60e8a5ab1b110f79d507",
+    "url": "/copyedit/static/media/Verdana-Bold.215accaa.ttf"
+  },
+  {
+    "revision": "236ce037aff36d5984aa6fabd1f32b61",
+    "url": "/copyedit/static/media/Verdana-Bold.236ce037.woff"
+  },
+  {
+    "revision": "742f68f6c36c131896a464c6bb0118bf",
+    "url": "/copyedit/static/media/Verdana-Bold.742f68f6.eot"
+  },
+  {
+    "revision": "7b92406001f9896d619169b573e946d1",
+    "url": "/copyedit/static/media/Verdana-Bold.7b924060.woff2"
+  },
+  {
+    "revision": "4b8e54294ffbdb4ecb4461fb69fe2415",
+    "url": "/copyedit/static/media/Verdana.4b8e5429.woff2"
+  },
+  {
+    "revision": "5587517d47893e1470ab2296eb609c93",
+    "url": "/copyedit/static/media/Verdana.5587517d.ttf"
+  },
+  {
+    "revision": "5748d62a838b6e8b85c34ac28f100587",
+    "url": "/copyedit/static/media/Verdana.5748d62a.eot"
+  },
+  {
+    "revision": "80d68a71e9bc56c0ea5c55bb6a63c24e",
+    "url": "/copyedit/static/media/Verdana.80d68a71.svg"
+  },
+  {
+    "revision": "977a9918c12f403b3da06022f90145df",
+    "url": "/copyedit/static/media/Verdana.977a9918.woff"
+  },
+  {
+    "revision": "ced06691a9c306bbe1c2d4991720c06d",
+    "url": "/copyedit/static/media/overhead-view-of-spiral-notepad-with-pen-and-pink-flowers-on-wooden-background_23-2147889664.ced06691.jpg"
+  }
+]);
